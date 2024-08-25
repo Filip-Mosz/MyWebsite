89791bf5e3782dd356a66e406977eaec2597e84b
Filip-Mosz <filip.moszczynski@onet.pl>
2024-08-25 10:01:29 +0200

    pushed missing changes

--- src/main/java/pl/filipmoszczynski/MyWebsite/HomeController.java
+++ src/main/java/pl/filipmoszczynski/MyWebsite/HomeController.java
@@ -1,13 +1,15 @@
 package pl.filipmoszczynski.MyWebsite;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
 
     @RequestMapping("/")
-    public String getHome(){
+    public String getHome(Model model){
+        model.addAttribute("pro", true);
 
         return "index.html";
     }
--- src/main/java/pl/filipmoszczynski/MyWebsite/MyWebsiteApplication.java
+++ src/main/java/pl/filipmoszczynski/MyWebsite/MyWebsiteApplication.java
@@ -11,3 +11,13 @@ public class MyWebsiteApplication {
 	}
 
 }
+/*
+* To Do
+* filbudrentexpol z opisem jak bardzo nie ogarniałem wtedy ile się od tego czasu zmieniło [podstrona na podstronie projekty]
+* pierdółki jako podstrona= liczby wesołe i takie tam śmieci
+* po umieszczeniu pierwszego sensownego projektu ogarnać ko-fi lub podobny serwis, żeby płynęły hajsy; znaczek pojawia się tylko tam, gdzie jest projekt, który może się przydać ludzikom
+* kalkulator łuczniczy jako podstrona projektów
+* agregator wyników minilotto jako podstrona ale bez linku w menu - dane zapisywać i odczytywać z pliku/ów wymyślić metodę transkrupcji
+* osobna podstrona na poradnik doboru i kompozycji stroju dla ChZG ze wspomnieniem dla kogo powstało i linkiem do ziemnieków
+* 	strony generowane z JSONa wkomponować obrazki(metoda interpretująca input) -> [Fig.01: podpis] -> <div><img src=Fig.01/> Fig.01 podpis </div>
+* */
